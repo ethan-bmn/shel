cc826e23e75597def263488a5df1607c1a0743f8
Ethan <ethan.bmn@hotmail.com>
2024-06-14 11:48:59 +0200

    Remove login requirement to access dashboard

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,10 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+Route::get('/', function () {
+    return Inertia::render('Dashboard');
+})->name('dashboard');
 Route::middleware('auth')->group(function () {
-    Route::get('/', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
 
     Route::get('/profile', [ProfileController::class, 'show'])
         ->name('profile.show');
