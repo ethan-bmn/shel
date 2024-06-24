b5ed349975fad1cbb720361d7446ee0207023f13
Ethan <ethan.bmn@hotmail.com>
2024-06-24 15:52:25 +0200

    Remove built-in components, update dashboard, create GameCard component (WIP)

--- resources/js/Components/Sidebar.vue
+++ resources/js/Components/Sidebar.vue
@@ -4,9 +4,13 @@
 
 <template>
 
-    <div class="col-2 navbar">
+    <div class="navbar">
         <div>construction in progress</div>
-        <img src="https://media.istockphoto.com/id/177370583/fr/photo/c%C3%B4nes-de-signalisation-et-casque-de-chantier-3d-isol%C3%A9.jpg?s=612x612&w=0&k=20&c=Mm0a5rzoOMcHY4f_sNzkzE-k126cG9WDuQfmx9Ch65c=" style="max-height: 200px;border-radius: 30px">
+        <img
+            src="https://media.istockphoto.com/id/177370583/fr/photo/c%C3%B4nes-de-signalisation-et-casque-de-chantier-3d-isol%C3%A9.jpg?s=612x612&w=0&k=20&c=Mm0a5rzoOMcHY4f_sNzkzE-k126cG9WDuQfmx9Ch65c="
+            class="img-fluid"
+            alt="..."
+        >
     </div>
 
 </template>
