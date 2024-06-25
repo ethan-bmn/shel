34c9b5197fadca61b82ef469aa26dcf8aaa6f911
Simon <simon.vende.pro@gmail.com>
2024-06-25 15:32:22 +0200

    End of edit of model

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Photo extends Model
 {
     use HasFactory;
+
+    public function boardgame(): BelongsTo
+    {
+        return $this->belongsTo('App\Models\Boardgame');
+    }
 }
--- app/Models/Photo_User.php
+++ app/Models/Photo_User.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Photo_User extends Model
 {
     use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo('App\Models\User');
+    }
 }
