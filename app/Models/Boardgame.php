fae34f961bd8b7ed0bd1968ffe5e659f1ca8af06
Simon <simon.vende.pro@gmail.com>
2024-06-25 11:58:33 +0200

    Edit of Models and creation of factories

--- app/Models/Boardgame.php
+++ app/Models/Boardgame.php
@@ -4,8 +4,37 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Boardgame extends Model
 {
     use HasFactory;
+
+    //Relation avec la table recommendation
+    public function recommendation(): HasMany
+    {
+        return $this->hasMany('App\Models\Recommendation');
+    }
+
+    public function photo(): HasMany
+    {
+        return $this->hasMany('App\Models\Photo');
+    }
+
+    public function shopping_cart(): BelongsTo
+    {
+        return $this->belongsTo('App\Models\Shopping_Cart');
+    }
+
+    public function category(): BelongsToMany
+    {
+        return $this->belongsToMany(category::class, 'categorize');
+    }
+
+    public function loan(): BelongsToMany
+    {
+        return $this->belongsToMany(loan::class, 'borrow');
+    }
 }
