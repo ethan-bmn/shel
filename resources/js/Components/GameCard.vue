b4071e31f2ca80a672c2449dba8ef607f11e0343
Ethan <ethan.bmn@hotmail.com>
2024-06-25 15:44:58 +0200

    Fix CSS for game card component

--- resources/js/Components/GameCard.vue
+++ resources/js/Components/GameCard.vue
@@ -16,17 +16,15 @@ defineProps({
 </script>
 
 <template>
-    <div class="image-container rounded-3">
-        <img :src="image" class="img-fluid z-0" alt="...">
+    <div class="image-container rounded-3 mx-2">
+        <img :src="image" class="img-fluid z-50" alt="...">
         <div class="hover-text">
-            <div class="card-body">
-                <h5 class="card-title text-light">
-                    {{ title }}
-                </h5>
-                <p class="card-text text-light">
-                    {{ body }}
-                </p>
-            </div>
+            <h5 class="card-title text-light">
+                {{ title }}
+            </h5>
+            <p class="card-text text-light">
+                {{ body }}
+            </p>
         </div>
     </div>
 </template>
@@ -34,21 +32,22 @@ defineProps({
 <style scoped>
 .image-container {
     overflow: hidden;
+    position: relative;
+    z-index: -1;
 }
 .hover-text {
     position: absolute;
-    top: 50%;
-    left: 50%;
     display: block;
-    transform: translate(-50%, -50%);
     color: white;
-    background-color: rgba(0, 0, 0, 0.7);
+    top: 0;
+    left: 0;
     width: 100%;
-    padding: 10px 20px;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.7);
+    padding: 10px 40px;
     border-radius: 5px;
     opacity: 0;
     transition: opacity 0.3s ease-in-out;
-    border: solid 1px red;
 }
 .image-container:hover .hover-text {
     opacity: 1;
