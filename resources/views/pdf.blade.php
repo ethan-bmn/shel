69f3a9872c3b07c2ee819738a1131b1f4a7df92b
Xanark <ldumasleo@gmail.com>
2024-06-27 14:55:00 +0200

    pdf style

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -1,35 +1,65 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel 10 Generate PDF Example - fundaofwebit.com</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Récapitulatif de Location - Shel</title>
+    <style>
+        .pdf__center{
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
 
-    <h1>{{ $title }}</h1>
-    <p>{{ $date }}</p>
-    <br/>
-    <br/>
+    <div class="pdf___header">
+        <!-- <img src="{{ public_path('images/logo_shel.png') }}"> -->
+        <h1>Récapitulatif de Location</h1>
+        <p>Magasin de location de jeux de société - Shel</p>
+        
+    </div>
 
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Created At</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>gfdhfd</td>
-                <td>ryhtj</td>
-                <td>mail</td>
-                <td>op</td>
-            </tr>
+    <div>
+        <p>Date de début de location: <strong>01/07/2024</strong></p>
+        <p>Date de fin de location: <strong>07/07/2024</strong></p>
+        <br/>
+        <h2>Détails de la location</h2>
+        <div class="pdf__center">
+            <p>client: leo dumas</p>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nom du Jeu</th>
+                        <th>Date de Début</th>
+                        <th>Date de Fin</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Catane</td>
+                    
+                        <td>01/07/2024</td>
+                        <td>07/07/2024</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Monopoly</td>
 
-        </tbody>
-    </table>
+                        <td>02/07/2024</td>
+                        <td>08/07/2024</td>
+                    </tr>
+                    <!-- Ajoutez plus de lignes ici si nécessaire -->
+                </tbody>
+            </table>
+        </div>
+        <br/>
+        <h2>Mentions Légales</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+
+    <div>
+        <p>Merci d'avoir choisi Shel pour vos locations de jeux de société !</p>
+    </div>
 
 </body>
 </html>
