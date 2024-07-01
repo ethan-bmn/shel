1b8be797c0e5c9cebe87ed146460c6e1b73de775
Simon <simon.vende.pro@gmail.com>
2024-07-01 10:33:11 +0200

    Small changes

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
                     $table->string('email')->unique();
                     $table->timestamp('email_verified_at')->nullable();
                     $table->string('password');
+                    $table->string('picture')->nullable();
                     $table->foreignId('role_id')->default(1)->constrained('roles')->onDelete('cascade');
                     $table->rememberToken();
                     $table->timestamps();
