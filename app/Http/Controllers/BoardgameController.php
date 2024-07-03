9e06fff9423e74586ea2bd35a49e2c3faa70da62
Ethan <ethan.bmn@hotmail.com>
2024-07-03 14:51:20 +0200

    Fix bug on game show page

--- app/Http/Controllers/BoardgameController.php
+++ app/Http/Controllers/BoardgameController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Boardgame;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
@@ -57,15 +58,17 @@ class BoardgameController extends Controller
     public function show(string $boardgame)
     {
         $jeu = Boardgame::find($boardgame);
-        $user_cart = DB::table('shopping_carts')
-            ->where('user_id', auth()->id())
-            ->first();
-        $put_on = DB::table('put_on')
-            ->where('boardgame_id', $jeu->id)
-            ->where('shopping_cart_id', $user_cart->id);
+        if (Auth::user()) {
+            $user_cart = DB::table('shopping_carts')
+                ->where('user_id', auth()->id())
+                ->first();
+            $put_on = DB::table('put_on')
+                ->where('boardgame_id', $jeu->id)
+                ->where('shopping_cart_id', $user_cart->id);
+        }
         return Inertia::render('Games', [
             'jeu' => $jeu,
-            'isInCart' => $put_on->exists(),
+            'isInCart' => $put_on && $put_on->exists(),
         ]);
     }
 
