2c3b3b2054365ff5210fabba4877f723915ff57e
Simon <simon.vende.pro@gmail.com>
2024-07-03 16:02:09 +0200

    small changes

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -80,11 +80,11 @@
         </div>
         <br/>
         <h2>Mentions Légales</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <p>Ce sont des mentions légales</p>
     </div>
 
     <div class="footer">
-        <p>Merci d'avoir choisi Shel pour vos locations de jeux de société !</p>
+        <p>Merci d'avoir choisis SHEL pour vos locations de jeux de société !</p>
     </div>
 
 </body>
